db1c8d8e43f338f5f05c1449bb16048f2d07eee1
AbdelmajidBen <abdelmajid.benjelloun@etu.uae.ac.ma>
2024-07-07 01:34:30 +0100

    1st day done

--- App.js
+++ App.js
@@ -4,14 +4,35 @@ import { Provider } from 'react-redux';
 import { store } from './store';
 import HomeScreen from './screens/HomeScreen';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import MapScreen from './screens/MapScreen';
+
 // 1- setup Redux
 
 export default function App() {
+  const Stack = createStackNavigator();
+
+
   return (
     <Provider store={store}>
+      <NavigationContainer>
       <SafeAreaProvider>
-      <HomeScreen />
+      <Stack.Navigator>
+        <Stack.Screen name="HomeScreen" component={HomeScreen}
+        options={{
+          headerShown: false,
+          }}
+           />
+        <Stack.Screen name="MapScreen" component={MapScreen}
+        options={{
+          headerShown: false,
+          }}
+           />
+      </Stack.Navigator>
       </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
 
   );
--- components/NavOptions.js
+++ components/NavOptions.js
@@ -2,6 +2,7 @@ import { FlatList, StyleSheet, Text, TouchableOpacity, View ,Image } from 'react
 import React from 'react'
 import tw from 'twrnc'
 import { Icon } from 'react-native-elements';
+import { useNavigation } from '@react-navigation/native';
 
 const data = [
   {
@@ -19,6 +20,9 @@ const data = [
 ]
 
 const NavOptions = () => {
+    const navigation = useNavigation();
+
+
     return (
       <FlatList
         data={data}
@@ -26,6 +30,8 @@ const NavOptions = () => {
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
           <TouchableOpacity
+            onPress={() => navigation.navigate(item.screen)}
+
           style={tw `p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
           >
             <View>
